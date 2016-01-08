3aba21e4dc8ae2d727be0326917b47a4da69bb35
Tang <nhtang111@163.com>
2016-01-08 10:25:27 +0800

    Add meter_info_update.

---
+++ modules/mod_meter_info_update/mod_meter_info_update.php
@@ -0,0 +1,37 @@
+﻿<?php 
+/**
+ * @package     electromonitor.com
+ * @subpackage  mod_meter_info_update
+ *
+ * @copyright   Copyright (C) 2016 All rights reserved.
+ */
+ 
+ //header('Content-type:text/html;charset=utf8');
+
+defined('_JEXEC') or die;
+
+// Include the functions only once
+require_once __DIR__ . '/helper.php';
+
+$info_id = trim(JRequest::getVar('info_id', '-1')); 
+
+if($info_id == "-1"){
+    mysql_close();
+    echo "<script>alert('请先修改记录！');history.back(); </script>";
+}else{
+	  
+	  $location_id = trim(JRequest::getVar('location_id', '-1'));
+	  $meter_address = trim(JRequest::getVar('meter_address', '-1'));
+	  $meter_model = trim(JRequest::getVar('meter_model', '-1'));
+	  
+		
+	  date_default_timezone_set('Asia/Singapore');
+      $datetime = date('Y-m-d H:i:s');
+      $datetime_change = $datetime;
+	  
+  ModMeterInfoUpdateHelper::updateMeterInfolValues($datetime_change, $info_id, $location_id, $meter_address, $meter_model );
+
+  require(JModuleHelper::getLayoutPath('mod_meter_info_update', 'default'));
+	
+}
+?>
