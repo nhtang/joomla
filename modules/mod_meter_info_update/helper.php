493a2659aac712abc4291148fc3f9d4be6152037
Tang <nhtang111@163.com>
2016-01-14 14:12:53 +0800

    switch and meter_info ok.Pm

--- modules/mod_meter_info_update/helper.php
+++ modules/mod_meter_info_update/helper.php
@@ -24,30 +24,32 @@ class ModMeterInfoUpdateHelper
                       //$query = 'SELECT * FROM joomla3_meter_info WHERE location_id ='.$location_id.' and location_id ='.$location_id;
 				
                 $db->setQuery($query);
-                //$result = $db->loadResult();
-				$row = $db->loadAssoc();      
+                $row = $db->loadResult();
+				//$row = $db->loadAssoc();      
 				
-            if($result != ""){
-					echo " <script>alert('数据库中已存在位置、表地址码相同的记录！ 记录序号：".$result."');history.back(); </script>";
+            if($row != ""){
+					echo " <script>alert('数据库中已存在位置、表地址码相同的记录！ 记录序号：".$row."');history.back(); </script>";
 			}else{
 
 				  // Get data source Meter_model ---------------------------------------- 
 		          $db = JFactory::getDBO();
                   $query = "SELECT * FROM joomla3_meter_info WHERE info_id = '$info_id' ";
                   $db->setQuery($query);
-				  $row_model = $db->loadAssoc();
-		          $meter_model_old = $row_model['meter_model'];
+				  $row_change = $db->loadAssoc();
+				  $location_id_old = $row_change['location_id'];
+				  $meter_address_old = $row_change['meter_address'];
+		          $meter_model_old = $row_change['meter_model'];
 				  //echo "<script>alert('原来的型号为：$meter_model_old ');history.back(); </script>";
 				  
 				  
 				  
-				if($meter_model_old != $meter_model){
+				if(($meter_model_old != $meter_model) || ($location_id_old != $location_id) || ($meter_address_old != $meter_address)){
 				      // Get $status_id ---------------------------------------- 
 		              $db = JFactory::getDBO();
-					  $query = "SELECT * FROM joomla3_electrical_status WHERE location_id = '$location_id' and meter_address = '$meter_address'";
+					  $query = "SELECT * FROM joomla3_electrical_status WHERE location_id = '$location_id_old' and meter_address = '$meter_address_old'";
                       $db->setQuery($query);
 				      $row_status = $db->loadAssoc();
-		              $status_id = $row_status['electrical_status'];
+		              $status_id = $row_status['status_id'];
 				  
 				      if($row_status == ""){ 
 				          echo "<script>alert('警告：数据库中没有此电表状态信息！');history.back(); </script>";
@@ -58,6 +60,8 @@ class ModMeterInfoUpdateHelper
 				        // if meter_model change set electrical_status  init 0
                         $profile_status = new stdClass();
 				        $profile_status->status_id = $status_id;
+						$profile_status->location_id = $location_id;
+						$profile_status->meter_address = $meter_address;
                         $profile_status->electrical_status = 0 ;
                
                         // Update the object from the user profile table.
@@ -66,7 +70,7 @@ class ModMeterInfoUpdateHelper
 				   
 				} //if($meter_model_old != $meter_model
 				
-				
+	
 				
 			 //Update Create and populate an object-------------------------
                 $profile = new stdClass();
@@ -90,3 +94,4 @@ class ModMeterInfoUpdateHelper
 
     }
 }
+?>
